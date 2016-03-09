cbf3c15970c1ebd45cace25519909740e45e347b
Jean Traullé <jtraulle@users.noreply.github.com>
2016-03-09 23:22:33 +0100

    Update start_pdf_worker.sh

--- pdf_worker/start_pdf_worker.sh
+++ pdf_worker/start_pdf_worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while : ; do
-    [[ -f "/var/www/html/vendor/autoload.php" ]] && break
+    [[ -f "/var/www/html/Vendor/autoload.php" ]] && break
     echo "Waiting for end of composer dependencies installation ..."
     sleep 1
 done
